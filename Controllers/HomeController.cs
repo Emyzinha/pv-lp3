fbfb0854eb3cf2f398181b8b7cbd61a4095c8083
Emyzinha <101672818+Emyzinha@users.noreply.github.com>
2022-09-27 21:48:19 -0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,16 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using provaLp3.Models;
+using lp3-av1.Models;
+using lp3-av1.ViewModels;
 
-namespace provaLp3.Controllers;
+
+namespace Av1.Controllers;
 
 public class HomeController : Controller
 {
+    private static List<Estabelecimento> estabelecimentos = new List<Estabelecimento>();
+
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -15,7 +20,59 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.estabelecimentos = estabelecimentos;
         return View();
+    }
+
+    public IActionResult Cadastro()
+    {
+        return View();
+    }
+
+    public IActionResult Cadastrar([FromForm] int id, [FromForm] int piso, [FromForm] string nome, [FromForm] string email, [FromForm] string descricao, [FromForm] bool tipo)
+    {
+        foreach (Estabelecimento estabelecimento in estabelecimentos)
+        {
+            if (estabelecimento.Id == id || estabelecimento.Nome == nome)
+            {
+                return Content("Estabelecimento já cadastrado");
+            }
+        }
+        estabelecimentos.Add(new Estabelecimento(id, piso, nome, descricao, email, tipo));
+        return View("Cadastro");
+    }
+
+    public IActionResult Gerenciamento()
+    {
+        ViewBag.estabelecimentos = estabelecimentos;
+        return View();
+    }
+
+    public IActionResult Remover([FromForm] int id)
+    {
+        foreach (Estabelecimento estabelecimento in estabelecimentos)
+        {
+            if (estabelecimento.Id == id)
+            {
+                estabelecimentos.Remove(estabelecimento);
+                return Content("Estabelecimento removido com sucesso");
+            }
+        }
+
+        return Content("Não foi possível remover");
+    }
+
+    public IActionResult Detalhes([FromForm] int id)
+    {
+        foreach (Estabelecimento estabelecimento in estabelecimentos)
+        {
+            if (estabelecimento.Id == id)
+            {
+                return View(estabelecimento);
+            }
+        }
+
+        return Content("Não é possível mostrar os detalhes");
     }
 
     public IActionResult Privacy()
